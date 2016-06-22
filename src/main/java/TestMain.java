1ba449610a4891fc4cc229aa5bc232e750087d5f
Pascal <plange@client-b3.gkt-oberhausen.home>
2016-06-22 19:27:15 +0200

    ADD: build.gradle and TestMain.java

---
+++ src/main/java/TestMain.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by plange on 22.06.16.
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Wurde von Gradle gebaut.");
+    }
+}
